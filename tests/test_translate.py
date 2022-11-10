b7039e41dd6a04fb6be0efb041caba043d0059f6
Julia Ivanova <murova47@gmail.com>
2022-11-11 01:27:44 +0800

    add allure

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,7 +1,15 @@
+import allure
+from allure_commons.types import Severity
 from project_mobile_autotests_for_yandex_translate.model import app
 
 
+@allure.tag("ui", "mobile")
+@allure.label('owner', 'juliamur')
+@allure.feature('UI')
+@allure.story('Text translation')
 class TestsTranslateText:
+    @allure.severity(Severity.BLOCKER)
+    @allure.title('Translation text from English to Russian')
     def test_translation_text_from_English_to_Russian(self):
         # PRECONDITION
         app.given_opened()
@@ -14,6 +22,8 @@ class TestsTranslateText:
         # THEN
         app.main_screen.should_be_translated_text('Я робот')
 
+    @allure.severity(Severity.NORMAL)
+    @allure.title('Switch language by button')
     def test_switch_language_by_button(self):
         # PRECONDITION
         app.given_opened()
@@ -29,9 +39,14 @@ class TestsTranslateText:
                        .translate_target_lang_should_be('ENGLISH')\
                        .should_be_translated_text('Milk')
 
-
+@allure.tag("ui", "mobile")
+@allure.label('owner', 'juliamur')
+@allure.feature('UI')
+@allure.story('Translation area')
 class TestsTranslationArea:
-    def test_clear_input_field_by_clicking_on_the_cross_icon(self):
+    @allure.severity(Severity.NORMAL)
+    @allure.title('Clear input field by click on the cross icon')
+    def test_clear_input_field_by_click_on_the_cross_icon(self):
         # PRECONDITION
         app.given_opened()
 
@@ -42,7 +57,9 @@ class TestsTranslationArea:
         # THEN
         app.main_screen.should_be_text_to_translate('Type text or a site address')
 
-    def test_open_fullscreen_translated_text(self):
+    @allure.severity(Severity.NORMAL)
+    @allure.title('Open translated text in full screen')
+    def test_open_translated_text_in_full_screen(self):
         # PRECONDITION
         app.given_opened()
 
@@ -55,8 +72,9 @@ class TestsTranslationArea:
                                         .should_have_text('Python - один из самых популярных языков программирования')\
                                         .tap_close_button()
 
-
-    def test_button_translate_site_should_be_visible(self):
+    @allure.severity(Severity.BLOCKER)
+    @allure.title('Button Translate site should be visible')
+    def test_button__translate_site__should_be_visible(self):
         # PRECONDITION
         app.given_opened()
 
